c164f676fcd1fa2353f2de5dbb5f790cfa973a8e
peu-94 <70969444+peu-94@users.noreply.github.com>
2025-07-28 23:59:49 +0530

    navbar components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
